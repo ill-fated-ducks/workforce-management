5a71efc0dd8d51ad0fe7730a92942bb590fb696a
arock83 <arock83@gmail.com>
2017-08-17 15:50:07 -0500

    pause to merge master

---
+++ Bangazon_Workforce_Management/Models/ComputerEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon_Workforce_Management.Models
+{
+    public class ComputerEmployee
+    {
+        [Key]
+        public int ComputerEmployeeID { get; set; }
+
+        [Required]
+        public int EmployeeID { get; set; }
+        
+        //public Employee Employee { get; set; }
+        
+        [Required]
+        public int ComputerID { get; set; }
+
+        public Computer Computer { get; set; }
+
+        [Required]
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+    }
+}
