afe1edf37dc9d26dacc75300c242cb2659570641
arock83 <arock83@gmail.com>
2017-08-17 14:50:16 -0500

    oops

--- Bangazon_Workforce_Management/Models/Computer.cs
+++ Bangazon_Workforce_Management/Models/Computer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,19 @@ namespace Bangazon_Workforce_Management.Models
 {
     public class Computer
     {
+        [Key]
+        public int ComputerID { get; set; }
+
+        [Required]
+        public DateTime PurchaseDate { get; set; }
+
+        public DateTime DecomissionDate { get; set; }
+
+        [Required]
+        public string Manufacturer { get; set; }
+        
+        [Required]
+        public string Make { get; set; }
+
     }
 }
