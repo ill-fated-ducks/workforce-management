67286d6db8f514a93eb7297361a129e05b47d0b2
arock83 <arock83@gmail.com>
2017-08-18 10:08:25 -0500

    editted department namespace and related files

--- Bangazon_Workforce_Management/Controllers/DepartmentsController.cs
+++ Bangazon_Workforce_Management/Controllers/DepartmentsController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Bangazon.Models;
 using Bangazon_Workforce_Management.Models;
 
 namespace Bangazon_Workforce_Management.Controllers
--- Bangazon_Workforce_Management/Models/Department.cs
+++ Bangazon_Workforce_Management/Models/Department.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 
-namespace Bangazon.Models
+namespace Bangazon_Workforce_Management.Models
 {
   public class Department
   {
