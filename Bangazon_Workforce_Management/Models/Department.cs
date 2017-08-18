b40ccf395e844594237be31975f4aecc9276e2ee
arock83 <arock83@gmail.com>
2017-08-18 09:30:38 -0500

    cleaned up my models

--- Bangazon_Workforce_Management/Models/Department.cs
+++ Bangazon_Workforce_Management/Models/Department.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bangazon.Models
 {
-  public class Departments
+  public class Department
   {
     [Key]
     public int DeptID {get;set;}
