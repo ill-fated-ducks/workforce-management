e5b8d12ce4940a081794d69a15f4ce0a8f2e9d7e
arock83 <arock83@gmail.com>
2017-08-18 10:13:31 -0500

    ctrls build now

--- Bangazon_Workforce_Management/Data/Bangazon_Workforce_ManagementContext.cs
+++ Bangazon_Workforce_Management/Data/Bangazon_Workforce_ManagementContext.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Bangazon.Models;
 using Bangazon_Workforce_Management.Models;
 
 namespace Bangazon_Workforce_Management.Models
@@ -17,7 +16,7 @@ namespace Bangazon_Workforce_Management.Models
 
         public DbSet<Bangazon_Workforce_Management.Models.Computer> Computer { get; set; }
 
-        public DbSet<Bangazon.Models.Department> Department { get; set; }
+        public DbSet<Bangazon_Workforce_Management.Models.Department> Department { get; set; }
 
         public DbSet<Bangazon_Workforce_Management.Models.TrainingProgram> TrainingProgram { get; set; }
 
