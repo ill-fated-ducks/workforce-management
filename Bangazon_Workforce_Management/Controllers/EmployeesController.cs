ea60c218dbb46ce16329ba99bc2d9a26fce7ba79
arock83 <arock83@gmail.com>
2017-08-18 09:51:11 -0500

    base controllers added

---
+++ Bangazon_Workforce_Management/Controllers/EmployeesController.cs
@@ -0,0 +1,159 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Bangazon_Workforce_Management.Models;
+
+namespace Bangazon_Workforce_Management.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly Bangazon_Workforce_ManagementContext _context;
+
+        public EmployeesController(Bangazon_Workforce_ManagementContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            var bangazon_Workforce_ManagementContext = _context.Employee.Include(e => e.Department);
+            return View(await bangazon_Workforce_ManagementContext.ToListAsync());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employee
+                .Include(e => e.Department)
+                .SingleOrDefaultAsync(m => m.EmployeeID == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            ViewData["DeptID"] = new SelectList(_context.Department, "DeptID", "DeptName");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EmployeeID,FirstName,LastName,StartDate,Supervisor,DeptID")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["DeptID"] = new SelectList(_context.Department, "DeptID", "DeptName", employee.DeptID);
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employee.SingleOrDefaultAsync(m => m.EmployeeID == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            ViewData["DeptID"] = new SelectList(_context.Department, "DeptID", "DeptName", employee.DeptID);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EmployeeID,FirstName,LastName,StartDate,Supervisor,DeptID")] Employee employee)
+        {
+            if (id != employee.EmployeeID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(employee.EmployeeID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            ViewData["DeptID"] = new SelectList(_context.Department, "DeptID", "DeptName", employee.DeptID);
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employee
+                .Include(e => e.Department)
+                .SingleOrDefaultAsync(m => m.EmployeeID == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employee = await _context.Employee.SingleOrDefaultAsync(m => m.EmployeeID == id);
+            _context.Employee.Remove(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return _context.Employee.Any(e => e.EmployeeID == id);
+        }
+    }
+}
---
+++ Bangazon_Workforce_Management/Data/Bangazon_Workforce_ManagementContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Bangazon.Models;
+using Bangazon_Workforce_Management.Models;
+
+namespace Bangazon_Workforce_Management.Models
+{
+    public class Bangazon_Workforce_ManagementContext : DbContext
+    {
+        public Bangazon_Workforce_ManagementContext (DbContextOptions<Bangazon_Workforce_ManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bangazon_Workforce_Management.Models.Computer> Computer { get; set; }
+
+        public DbSet<Bangazon.Models.Department> Department { get; set; }
+
+        public DbSet<Bangazon_Workforce_Management.Models.TrainingProgram> TrainingProgram { get; set; }
+
+        public DbSet<Bangazon_Workforce_Management.Models.Employee> Employee { get; set; }
+    }
+}
