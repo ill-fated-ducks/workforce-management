6e503307dc5385fe67077d32722121c5cadc8797
unknown <mitchellblom@gmail.com>
2017-08-18 13:44:31 -0500

    new controllers with join collection lines

---
+++ Bangazon_Workforce_Management/Migrations/20170818184153_NewControllers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bangazon_Workforce_Management.Migrations
+{
+    public partial class NewControllers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
