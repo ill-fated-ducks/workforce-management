3be3fe621be91562ef3fb6d4e1ef1c5cb24b5593
azimsodikov <azimsadikov@gmail.com>
2017-08-18 21:30:32 -0500

    Fixed Typos

--- Bangazon_Workforce_Management/Controllers/HomeController.cs
+++ Bangazon_Workforce_Management/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MvcMBangazon_Workforce_Managementovie.Controllers
+namespace Bangazon_Workforce_Management.Controllers
 {
     public class HomeController : Controller
     {
