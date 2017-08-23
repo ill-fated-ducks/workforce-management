b87b8ba666ca328144b5579dad98fa5e15cb089a
azimsodikov <azimsadikov@gmail.com>
2017-08-23 10:38:44 -0500

    Getting the right data

---
+++ Bangazon_Workforce_Management/Models/ViewModel/EmployeeEditVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon_Workforce_Management.Models.ViewModel
+{
+    public class EmployeeEditVM
+    {
+        public Employee Employee { get; set; }
+        public List<TrainingProgram> TrainingPrograms { get; set; }
+        public List<Computer> Computers { get; set; }
+        public EmployeeEditVM()
+        {
+            TrainingPrograms = new List<TrainingProgram>();
+            Computers = new List<Computer>();
+            ComputerEmployee = new ComputerEmployee();
+            TrainingProgramEmployee = new TrainingProgramEmployee();
+        }
+        public int ComputerID { get; set; }
+        public int DepID { get; set; }
+        public ComputerEmployee ComputerEmployee { get; set; }
+        public TrainingProgramEmployee TrainingProgramEmployee { get; set; }
+    }
+}
