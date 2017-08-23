d86f2a1f3c9a59d263fa1a9acb87199e171626a1
arock83 <arock83@gmail.com>
2017-08-23 09:30:40 -0500

    employees show on home page

--- Bangazon_Workforce_Management/Controllers/HomeController.cs
+++ Bangazon_Workforce_Management/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Bangazon_Workforce_Management.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bangazon_Workforce_Management.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly Bangazon_Workforce_ManagementContext _context;
+
+        public HomeController(Bangazon_Workforce_ManagementContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var bangazon_Workforce_ManagementContext = _context.Employee.Include(e => e.Department);
+            return View(await bangazon_Workforce_ManagementContext.ToListAsync());
+            
         }
 
         public IActionResult About()
